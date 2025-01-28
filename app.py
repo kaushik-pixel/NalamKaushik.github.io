2d471f41d5e713100915add96c9c62c57d87b862
kaushik-pixel <70168894+kaushik-pixel@users.noreply.github.com>
2025-01-28 17:21:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,11 @@ app = Flask(__name__, static_folder="frontend", static_url_path="/frontend")
 
 @app.route("/")
 def serve_frontend():
-    return send_from_directory("../frontend", "index.html")
-
+    return send_from_directory("frontend", "index.html")
+    
+@app.route("/test")
+def test():
+    return "Backend is working!"
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
